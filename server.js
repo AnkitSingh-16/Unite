07d1bd99098ad69408789b43b50e216fdf1e2435
Ankit Singh <2808aksingh@gmail.com>
2023-05-18 22:59:52 +0530

    Deploy

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async(req, res, next) =>{
    console.log(req.files)
    let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-   res.redirect(`http://localhost:3000/static/${d}.pdf`)
+   res.redirect(`https://unite-n04g.onrender.com/static/${d}.pdf`);
 })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening on port https://unite-n04g.onrender.com`);
 }) 
